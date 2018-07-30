958b59a31e378b5b191db1030c36013b998d4b23
Grey Li <withlihui@gmail.com>
2018-07-30 10:32:07 +0800

    Use Bootstrap-Flask to integrate Bootstrap

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -6,6 +6,7 @@
     :license: MIT, see LICENSE for more details.
 """
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('sayhello')
@@ -14,5 +15,6 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 
 from sayhello import views, errors, commands
