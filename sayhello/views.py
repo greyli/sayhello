85c3ad9bd47505d29dbc0454b7a3800b15d4b98c
Grey Li <withlihui@gmail.com>
2019-06-05 18:51:59 +0800

    Reorder code in index()

--- sayhello/views.py
+++ sayhello/views.py
@@ -14,7 +14,6 @@ from sayhello.models import Message
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    messages = Message.query.order_by(Message.timestamp.desc()).all()
     form = HelloForm()
     if form.validate_on_submit():
         name = form.name.data
@@ -24,4 +23,6 @@ def index():
         db.session.commit()
         flash('Your message have been sent to the world!')
         return redirect(url_for('index'))
+
+    messages = Message.query.order_by(Message.timestamp.desc()).all()
     return render_template('index.html', form=form, messages=messages)
